b70f5e9345e06b4fecd16e97a2216c2ec62f89b0
Siarhei Lunski <loonskai@gmail.com>
2021-02-18 17:09:47 +0300

    Add inert polyfill

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 const modal = document.querySelector('#modal');
-const modalFade = document.querySelector('.modal-fade');
+const modalMask = document.querySelector('.modal-mask');
 const loginBtn = document.querySelector('#login-btn');
 
 const KEYS = {
@@ -7,23 +7,40 @@ const KEYS = {
   TAB: 'Tab',
 };
 
-// TODO: Implement focus trap on modal
+// MODAL
+loginBtn.addEventListener('click', openModal);
+let previousActiveElement;
 
-function closeOnClick(key) {
-  return function(e) {
-    console.dir(e)
-    if (e.key === key) {
-      modal.style.display = 'none';
-    }
+function openModal() {
+  previousActiveElement = document.activeElement;
+  Array.from(document.body.children).forEach(child => {
+    if (child !== modal) child.inert = true;
+  });
+
+  modal.classList.remove('non-visible');
+  modal.classList.add('visible');
+  modalMask.addEventListener('click', closeModal);
+  document.addEventListener('keydown', checkCloseDialog);
+
+  modal.focus();
+}
+
+function closeModal() {
+  modalMask.removeEventListener('click', closeModal);
+  document.removeEventListener('keydown', checkCloseDialog);
+  Array.from(document.body.children).forEach(child => {
+    child.inert = false;
+  });
+
+  modal.classList.add('non-visible');
+  modal.classList.remove('visible');
+
+  previousActiveElement.focus();
+}
+
+function checkCloseDialog(e) {
+  if (e.key === KEYS.ESC) {
+    closeModal();
   }
 }
 
-loginBtn.addEventListener('click', e => {
-  modal.style.display = '';
-  modal.focus();    
-  document.addEventListener('keydown', closeOnClick(KEYS.ESC), { once: true });
-});
-
-modalFade.addEventListener('click', e => {
-  modal.style.display = 'none';
-})
